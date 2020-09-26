935da21e2382fa0b40eafda62e583dfc89858890
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-26 23:51:30 +0200

    Added Accordion component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 
 import { JumbotronContainer } from './containers/JumbotronContainer';
 import { FooterContainer } from './containers/FooterContainer';
+import { FaqsContainer } from './containers/FaqsContainer';
 
 const App = () => {
   return (
     <Fragment>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </Fragment>
   );
