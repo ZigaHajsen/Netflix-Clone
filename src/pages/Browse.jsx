c475a5ccd50854e8133ccee50f08d0138e119fbd
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-28 23:37:07 +0200

    Added selectionFilter utility

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useContent } from '../hooks';
 
+import { selectionFilter } from '../utils';
+
 export default function Browse() {
   const { series } = useContent('series');
   const { films } = useContent('films');
+
+  const slides = selectionFilter({ series, films });
+  console.log(slides);
 
   return <p>Browse</p>;
 }
