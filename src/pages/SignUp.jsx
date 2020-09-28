3051c607e9a29c4f317c0cfb29926236b018c1c3
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-28 14:22:21 +0200

    Added Firebase SignUp

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,7 +7,7 @@ import { FirebaseContext } from '../context/firebase';
 
 import { HeaderContainer } from '../containers/HeaderContainer';
 import { FooterContainer } from '../containers/FooterContainer';
-import { Footer, Form } from '../components';
+import { Form } from '../components';
 
 export default function SignUp() {
   const [firstName, setFirstName] = useState('');
@@ -23,6 +23,26 @@ export default function SignUp() {
 
   const handleSignUp = (event) => {
     event.preventDefault();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          })
+      )
+      .catch((error) => {
+        setFirstName('');
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      });
   };
 
   return (
