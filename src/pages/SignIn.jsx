d5e620f688928d2f01a0d3ee6c74ceb42ec1e76c
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-28 12:14:25 +0200

    Added Form component & SignIn page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,56 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+
+import * as ROUTES from '../constants/routes';
+
+import { HeaderContainer } from '../containers/HeaderContainer';
+import { FooterContainer } from '../containers/FooterContainer';
+import { Form } from '../components';
 
 export default function SignIn() {
-  return <p>SignIn</p>;
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Fragment>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} method='POST'>
+            <Form.Input
+              type='email'
+              placeholder='Email adress'
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type='submit'>
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflix?{' '}
+            <Form.Link to={ROUTES.SIGN_UP}>Sign up now</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </Fragment>
+  );
 }
