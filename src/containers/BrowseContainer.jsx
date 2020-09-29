f850532c061c4328bde6cf93cb56d814f195d286
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-29 12:17:04 +0200

    Added Loading component

--- src/containers/BrowseContainer.jsx
+++ src/containers/BrowseContainer.jsx
@@ -1,13 +1,30 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 
 import { FirebaseContext } from '../context/firebase';
 
 import { SelectProfilesContainer } from './SelectProfilesContainer';
+import { Loading } from '../components';
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
 
-  return <SelectProfilesContainer user={user} setProfile={setProfile} />;
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return profile.displayName ? (
+    loading ? (
+      <Loading src={user.photoURL} />
+    ) : (
+      <Loading.ReleaseBody />
+    )
+  ) : (
+    <SelectProfilesContainer user={user} setProfile={setProfile} />
+  );
 }
